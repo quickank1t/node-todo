150b0e858427ea390883026ddbf585e3ae347df3
ankit <a.chaurasia@sap.com>
2024-03-12 19:26:18 +0100

    dot env and logger

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "dotenv/config";
+
+import { log } from "./utils/logger.js";
+
+log.info("started server");
---
+++ src/utils/logger.js
@@ -0,0 +1,5 @@
+import log from "loglevel";
+
+log.setLevel(process.env.LOG_LEVEL);
+
+export { log };
