b1500ae8e4d4896c94c3ce5b4011f1878e784c50
ankit <a.chaurasia@sap.com>
2024-03-12 20:18:51 +0100

    hello

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ hello: "world" });
 });
 
 log.trace(config);
